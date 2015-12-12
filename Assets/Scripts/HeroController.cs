00e208c1944541303a7d56d358b14648fa3ce2ec
Oleksii Kyrylchuk <olety7@gmail.com>
2015-12-12 03:41:48 +0100

    Added a hero that moves automatically and made the camera follow him
    Main:
    - Added a HeroController script that moves it in the right direction
    - Added a CameraController script that makes it so the camera follows the plaeyr
    Misc:
    - Added a hero.png sprite
    - Added a Scripts folder

---
+++ Assets/Scripts/HeroController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroController : MonoBehaviour {
+	public float horSpeed;
+	private Rigidbody2D rigidbody;
+
+	void Awake () {
+		this.rigidbody = this.GetComponent<Rigidbody2D> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		Vector2 velocity = new Vector2 ();
+		velocity.x += horSpeed;
+		this.rigidbody.velocity = velocity;
+
+	}
+
+}
